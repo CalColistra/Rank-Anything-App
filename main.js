d7226ce5c01f2d661e5fd523d480bf98c82a1bc6
Cal Colistra <calcolistra10@gmail.com>
2023-02-18 16:35:03 -0500

    added feature to post about bands along with songs added up and down arrows and fixed some bugs in the post page

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ class User {
 
 //post class:
 class Post {
-  constructor(userEmail, userName, rank, text, objectName, objectCreator, objectType, objectGenre) {
+  constructor(userEmail, userName, rank, text, objectName, objectType) {
     this.userEmail = userEmail;
     this.userName = userName;
     this.rank = rank;
@@ -33,9 +33,7 @@ class Post {
     this.upvotes = 0;
     this.downvotes = 0;
     this.objectName = objectName;
-    this.objectCreator = objectCreator;
     this.objectType = objectType;
-    this.objectGenre = objectGenre;
   }
 }
 
@@ -196,12 +194,14 @@ rankBtn.addEventListener('click', async e => {
         //console.log(matchingResults);
         let resultsList = "";
         for (let i=0; i < matchingResults.length; i++) {
-          resultsList += "<div><button id = '"+ matchingIds[i]+"' class = 'resultObject'><strong>";
-          resultsList += matchingResults[i].name + "</strong></br>"+matchingResults[i].creator;
-          resultsList += "</button><div/>";
+          resultsList += "<div><button id = '"+matchingIds[i]+"' class = 'resultObject'><strong>";
+          resultsList += matchingResults[i].name + "</strong>";
+
+          //resultsList += "</br>"+matchingResults[i].creator;
+          resultsList += "</button></div>";
         }
         searchResults.innerHTML = resultsList;
-        addListenersPost(matchingIds,matchingResults);
+        addListenersPost(matchingIds);
       }
       
     })
@@ -209,28 +209,35 @@ rankBtn.addEventListener('click', async e => {
   
 })
 
-async function addListenersPost(matchingIds, matchingResults) {
-  var userChoiceName, userChoiceCreator, userChoiceType, userChoiceGenre;
+async function addListenersPost(matchingIds) {
   for (let i=0; i < matchingIds.length; i++) {
-    var currentId = "#" + matchingIds[i];
-    //console.log(currentId);
+    var objectId = matchingIds[i];
+    //console.log(objectId);
+    var currentId = "#" + objectId;
     var currentResult = document.querySelector(currentId);
-    //console.log(currentResult);
-    currentResult.addEventListener('click', async e => {
-      e.preventDefault();
-      userChoiceName = matchingResults[i].name;
-      userChoiceCreator = matchingResults[i].creator;
-      userChoiceType = matchingResults[i].type;
-      userChoiceGenre = matchingResults[i].genre;
-      updatePostForm(userChoiceName, userChoiceCreator, userChoiceType, userChoiceGenre, matchingIds[i]);
-    })
+    setUpListener(currentResult, objectId);
   }
 }
 
-async function updatePostForm(userChoiceName, userChoiceCreator, userChoiceType, userChoiceGenre, objectId){
-  var postFormString = "<div> <strong>"+userChoiceName;
+async function setUpListener(currentResult, objectId) {
+  console.log("setting up listener for object: " +objectId);
+  currentResult.addEventListener('click', async e => {
+    e.preventDefault();
+    updatePostForm(objectId);
+  })
+}
+
+async function updatePostForm(objectId){
+  const objectRef = await getDoc(doc(db, "objects", objectId));
+  const type = objectRef.data().type;
+  const name = objectRef.data().name;
+  var postFormString = "<div> <strong>"+name;
       postFormString += "</strong>";
-      postFormString += " by " + userChoiceCreator +"</div>";
+      if ((type == "Song") || (type == "Book") || (type == "Film")) {
+        const creator = objectRef.data().creator;
+        postFormString += " by " + creator;
+      }
+      postFormString += "</div>";
       postFormString += "<div id='scaleInput'> <div id = 'postLabel'>Your rank on 0-5 scale: </div>";
       postFormString += "<input type='range' min='0' max='50' value='0' id='rankInput'>";
       postFormString += "<div id='displaySliderValue'> </div>";
@@ -242,10 +249,10 @@ async function updatePostForm(userChoiceName, userChoiceCreator, userChoiceType,
       postFormString += "</div>";
       postFormString += "</br><button id='publish"+objectId+"' class='publishPostButton' type='submit'>Publish</button></br>";
       postForm.innerHTML = postFormString;
-      handlePostInput(userChoiceName, userChoiceCreator, userChoiceType, userChoiceGenre, objectId);
+      handlePostInput(name, type, objectId);
 }
 
-async function handlePostInput(objectName, objectCreator, type, genre, objectId) {
+async function handlePostInput(name, type, objectId) {
   const slider = document.querySelector("#rankInput");
   const displaySliderValue = document.querySelector("#displaySliderValue");
   const publishId = "#publish"+objectId;
@@ -261,8 +268,7 @@ async function handlePostInput(objectName, objectCreator, type, genre, objectId)
   const textArea = document.querySelector("#textArea");
   submitPostBtn.addEventListener('click', async e =>{
     e.preventDefault();
-    //check if user has already made a post for this object**********
-    
+    //check if user has already made a post for this object:
     const objectRef = await getDoc(doc(db, "objects", objectId));
     if (objectRef.exists()) {
       const docSnap = objectRef.data();
@@ -270,38 +276,67 @@ async function handlePostInput(objectName, objectCreator, type, genre, objectId)
       const postIds = docSnap.postIds;
       //console.log(postIds);
       if (postIds.includes(currentUser.userEmail)) {
-        alert("You have already made a post for " + objectName);
+        alert("You have already made a post for " + name);
       }
       else {
         await updateDoc(doc(db,"objects", objectId), {
           postCount: postCount + 1,
           postIds: arrayUnion(currentUser.userEmail)
         })
-        var userText = textArea.value;
-        var userPost = new Post(currentUser.userEmail, currentUser.userName, sliderValue, userText, objectName, objectCreator, type, genre);
-        //console.log(userPost.text);
-        alert("You have successfully published your post about: " + objectName);
-        
-        const newPostRef = await addDoc(collection(db, "posts"), {
-          publisher: userPost.userEmail,
-          publisherName: userPost.userName,
-          rank: userPost.rank,
-          text: userPost.text,
-          objectName: userPost.objectName,
-          objectCreator: userPost.objectCreator,
-          type: userPost.objectType,
-          genre: userPost.objectGenre,
-          upvotes: userPost.upvotes,
-          downvotes: userPost.downvotes,
-          objectId: objectId
-        });
-        const newPostId = newPostRef.id;
-        currentUser.postCount = currentUser.postCount+1;
-        currentUser.postIds.push(newPostId);
-        await updateDoc(doc(db,"users", currentUser.userEmail), {
-          postCount: currentUser.postCount,
-          postIds: arrayUnion(newPostId)
-        })
+        if ((type == "Song") || (type == "Book") || (type == "Film")) {
+          var userText = textArea.value;
+          var objectCreator = docSnap.creator;
+          var genre = docSnap.genre;
+          var userPost = new Post(currentUser.userEmail, currentUser.userName, sliderValue, userText, name, type, genre);
+          //console.log(userPost.text);
+          alert("You have successfully published your post about: " + name);
+          
+          const newPostRef = await addDoc(collection(db, "posts"), {
+            publisher: userPost.userEmail,
+            publisherName: userPost.userName,
+            rank: userPost.rank,
+            text: userPost.text,
+            objectName: userPost.objectName,
+            objectCreator: objectCreator,
+            type: userPost.objectType,
+            genre: genre,
+            upvotes: userPost.upvotes,
+            downvotes: userPost.downvotes,
+            objectId: objectId
+          });
+          const newPostId = newPostRef.id;
+          currentUser.postCount = currentUser.postCount+1;
+          currentUser.postIds.push(newPostId);
+          await updateDoc(doc(db,"users", currentUser.userEmail), {
+            postCount: currentUser.postCount,
+            postIds: arrayUnion(newPostId)
+          })
+        }
+        else if ((type == "Band") || (type == "Artist") || (type == "Author") || (type == "Actor")) {
+          var userText = textArea.value;
+          var userPost = new Post(currentUser.userEmail, currentUser.userName, sliderValue, userText, name, type);
+          //console.log(userPost.text);
+          alert("You have successfully published your post about: " + name);
+          
+          const newPostRef = await addDoc(collection(db, "posts"), {
+            publisher: userPost.userEmail,
+            publisherName: userPost.userName,
+            rank: userPost.rank,
+            text: userPost.text,
+            objectName: userPost.objectName,
+            type: userPost.objectType,
+            upvotes: userPost.upvotes,
+            downvotes: userPost.downvotes,
+            objectId: objectId
+          });
+          const newPostId = newPostRef.id;
+          currentUser.postCount = currentUser.postCount+1;
+          currentUser.postIds.push(newPostId);
+          await updateDoc(doc(db,"users", currentUser.userEmail), {
+            postCount: currentUser.postCount,
+            postIds: arrayUnion(newPostId)
+          })
+        }
       }
     }
     else {
@@ -428,6 +463,9 @@ async function displayProfileSection() {
     displayProfilePosts();
 }
 
+
+
+
 async function displayProfilePosts() {
   //console.log(currentUser.postIds);
   var profilePostsString = "";
@@ -437,24 +475,42 @@ async function displayProfilePosts() {
     let postRef = doc(db, "posts", currentId);
     let docSnap = await getDoc(postRef);
     if (docSnap.exists()) {
+      
       profilePostsString += "<div id='"+currentId+"' class = 'aPost'>";
-        profilePostsString += "<div id='Rank"+currentId+"' class='displayRank'><div id = 'rankPostLabel"+currentId+"' class='profilePostLabel'>";
-        profilePostsString += "Rank:</div><div class='rankValue'>"+docSnap.data().rank+"</div></div>";
-        profilePostsString += "<div id = 'postContent'>";
-            profilePostsString += "<div id = 'leftPostContent'><div id = 'titlePostLabel"+currentId+"' class='profilePostLabel'>Title:</div><a href='#' id='titleValue"+currentId+"' class='titleValue'>";
-            profilePostsString += docSnap.data().objectName+" by "+docSnap.data().objectCreator;
-            profilePostsString += "</a></div>";
-            let upvotes = docSnap.data().upvotes;
-            let downvotes = docSnap.data().downvotes;
-            let votes = upvotes - downvotes;
-            profilePostsString += "<div id = 'rightPostContent'><div id = 'votesPostLabel"+currentId+"' class='profilePostLabel'>Votes:</div><div id='voteValue'>";
-            profilePostsString += votes+"</div></div>";
-            profilePostsString += "<div id = 'leftPostContent'><div id = 'textPostLabel"+currentId+"' class='profilePostLabel'>Text:</div> <div id='textContainer"+currentId+"' class='textContainer'> <div id='"+currentId+"textValue' class='textValue'>";
-            profilePostsString += docSnap.data().text+"</div></div></div>";
-            profilePostsString += "<div id = 'deleteBtnContainer"+currentId+"'> <button id='deletePost"+currentId+"'>Delete Post</button>";
-            profilePostsString += "<button id='editPost"+currentId+"'><img id='editIcon' src='/assets/editIcon.5de888a8.png'>Edit Post</button></div>"; 
+      profilePostsString += "<div id='Rank"+currentId+"' class='displayRank'><div id = 'rankPostLabel"+currentId+"' class='profilePostLabel'>";
+      profilePostsString += "Rank:</div><div class='rankValue'>"+docSnap.data().rank+"</div></div>";
+      profilePostsString += "<div id = 'postContent'>";
+      const objectType = docSnap.data().type;
+      if ((objectType == "Song") || (objectType == "Film") || (objectType == "Book")) {
+        profilePostsString += "<div id = 'leftPostContent'><div id = 'titlePostLabel"+currentId+"' class='profilePostLabel'>Title:</div><a href='#' id='titleValue"+currentId+"' class='titleValue'>";
+        profilePostsString += docSnap.data().objectName+" by "+docSnap.data().objectCreator;
+        profilePostsString += "</a></div>";
+      }
+      else if ((objectType == "Band") ||(objectType == "Artist") || (objectType == "Author") || (objectType == "Actor")) {
+        profilePostsString += "<div id = 'leftPostContent'><div id = 'titlePostLabel"+currentId+"' class='profilePostLabel'>Title:</div><a href='#' id='titleValue"+currentId+"' class='titleValue'>";
+        profilePostsString += docSnap.data().objectName;
+        profilePostsString += "</a></div>";
+      }
+          
+          let upvotes = docSnap.data().upvotes;
+          let downvotes = docSnap.data().downvotes;
+          let votes = upvotes - downvotes;
+          profilePostsString += "<div id = 'rightPostContent'><div id = 'votesPostLabel"+currentId+"' class='profilePostLabel'>Votes:</div>";
+          profilePostsString += "<div class='voteContainer'>";
+            profilePostsString += "<div class='voteButtonContainer'>";
+              profilePostsString += "<button class='voteArrowBtn' id = 'upVoteBtn"+currentId+"'> <img class='voteArrow' id='upArrow' src='https://raw.githubusercontent.com/CalColistra/Rank-Anything-App/master/img/upArrowIcon.png'> </button>";
+              profilePostsString += "<button class='voteArrowBtn' id = 'downVoteBtn"+currentId+"'> <img class='voteArrow' src='https://raw.githubusercontent.com/CalColistra/Rank-Anything-App/master/img/downArrowIcon.png'> </button>";
             profilePostsString += "</div>";
-      profilePostsString += "</div>";
+          profilePostsString += "<div class='voteValue'>"+votes+"</div>";
+          profilePostsString += "</div></div>";
+          profilePostsString += "<div id = 'leftPostContent'><div id = 'textPostLabel"+currentId+"' class='profilePostLabel'>Text:</div> <div id='textContainer"+currentId+"' class='textContainer'> <div id='"+currentId+"textValue' class='textValue'>";
+          profilePostsString += docSnap.data().text+"</div></div></div>";
+          profilePostsString += "<div id = 'deleteBtnContainer"+currentId+"'> <button id='deletePost"+currentId+"'>Delete Post</button>";
+          profilePostsString += "<button id='editPost"+currentId+"'><img id='editIcon' src='/assets/editIcon.5de888a8.png'>Edit Post</button></div>"; 
+          profilePostsString += "</div>";
+      profilePostsString += "</div>";        
+      
+      
     }
   }
   profilePosts.innerHTML = profilePostsString;
@@ -613,16 +669,42 @@ async function displayObjectPopup(objectId) {
   const averageRank = rankTotals/objectPostCount;
   const topDisplay = document.querySelector("#objectPopupTop");
   const bottomDisplay = document.querySelector("#objectPopupBottom");
+  const type = docSnap.data().type;
   var topString;
   topString = "<div id='leftObjectContent'><div class='objectLabel'>Average Rank:</div>";
   topString += "<div class='averageRank'>"+averageRank+"</div>";
   topString += "</div>";
   topString += "<div id='rightObjectContent'><div class='objectLabel'>Title:</div>";
-  topString += "<div class='objectLabel'>Release Date:</div>";
-  topString += "<div class='titleValue'><strong>"+docSnap.data().name+"</strong></br>";
-  topString += "&nbsp;&nbsp;&nbsp;<a href='#'>"+docSnap.data().creator+"</a>";
+  if ((type == "Song") || (type == "Film") || (type == "Book")) {
+    topString += "<div class='objectLabel'>Release Date:</div>";
+  }
+  else if ((type == "Band")) {
+    topString += "<div class='objectLabel'>Established:</div>";
+  }
+  else if ((type == "Artist") || (type == "Author") || (type == "Actor")) {
+    topString += "<div class='objectLabel'>Born:</div>";
+  }
+ 
+  topString += "<div class='titleValue'><strong>"+docSnap.data().name+"</strong>";
+ 
+  if ((type == "Song") || (type == "Film") || (type == "Book")) {
+    topString += "</br>&nbsp;&nbsp;&nbsp;<a id='creator"+objectId+"' href='#'>"+docSnap.data().creator+"</a>";
+  }
+  else if ((type == "Band") || (type == "Artist") || (type == "Author") || (type == "Actor")) {
+    topString += "&nbsp;&nbsp;&nbsp;<a href='#'></a>";
+  }
   topString += "</div>";
-  topString += "<div><strong>"+docSnap.data().releaseDate+"</strong></br>";
+  if ((type == "Song") || (type == "Film") || (type == "Book")) {
+    topString += "<div><strong>"+docSnap.data().releaseDate+"</strong></br>";
+  }
+  else if ((type == "Band")) {
+    topString += "<div><strong>"+docSnap.data().established+"</strong></br>";
+  }
+  else if ( (type == "Artist") || (type == "Author") || (type == "Actor")) {
+    topString += "<div><strong>"+docSnap.data().born+"</strong></br>";
+  }
+ 
+ 
   if (objectPostCount == 1) var haveRanked = "<strong>"+objectPostCount+"</strong> user has ranked "+docSnap.data().name;
   else var haveRanked = "<strong>"+objectPostCount+"</strong> users have ranked "+docSnap.data().name;
   topString += haveRanked;
@@ -635,6 +717,7 @@ async function displayObjectPopup(objectId) {
   for (let i = 0; i < postIds.length; i++) {
     var currentPostId = postIds[i];
     let postSnap = await getDoc(doc(db, "posts", currentPostId));
+    bottomString += "<div class='displayPublisherName' id='publisherName"+postSnap.data().publisher+"'>"+postSnap.data().publisherName+"</div>";
     bottomString += "<div id ='objectPost"+currentPostId+"' class='aPost'>";
       
 
